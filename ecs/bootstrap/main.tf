6ae1164954a4a6466f2ba26f0f501ac56efa2220
Md. Rajib Hossain Pavel <135492408+rajib-reea@users.noreply.github.com>
2025-07-10 19:39:16 +0600

    Create main.tf

---
+++ ecs/bootstrap/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "your-profile"
+  region  = "us-east-1"
+}
+
+resource "aws_kms_key" "terraform_state" {
+  description             = "KMS key for Terraform state encryption"
+  enable_key_rotation     = true
+  deletion_window_in_days = 10
+}
+
+output "kms_key_id" {
+  value = aws_kms_key.terraform_state.id
+}
