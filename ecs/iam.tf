0f1827bf05e1782ac88031e882918f8dd4a99c0d
Md. Rajib Hossain Pavel <135492408+rajib-reea@users.noreply.github.com>
2025-07-11 00:29:36 +0600

    Update iam.tf

--- ecs/iam.tf
+++ ecs/iam.tf
@@ -9,20 +9,6 @@ variable "secret_name" {
   type        = string
 }
 
-# 👥 Trust relationship document shared by both ECS roles
-data "aws_iam_policy_document" "ecs_assume_role_policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 # 🧠 Task Role (used by your app inside ECS container)
 resource "aws_iam_role" "ecs_task_role" {
   name               = "ecsTaskRole"
